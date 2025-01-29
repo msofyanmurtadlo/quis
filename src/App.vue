be2309ef1fd5b70f35e6c7fcc04743f9d5fd6ca4
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-01-30 03:45:12 +0700

    Feat: Menambahkan data dan menambahkan fitur search

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
+<script setup>
+
+import { ref, watch } from 'vue';
+import srcQuiz from './data/quizes.json';
+
+const quizes = ref(srcQuiz);
+const search = ref('');
+
+watch(search, () => {
+	quizes.value = srcQuiz.filter((quiz) => {
+		return quiz.title.toLowerCase().includes(search.value.toLowerCase());
+	});
+});
+
+
+</script>
+
 <template>
   <main>
     <header>
       <h1 id="title">Quiz</h1>
-      <input type="text" id="search-input" />
+      <input v-model.trim="search" type="text" id="search-input" />
     </header>
     <section id="quis-container">
-      <div class="card">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img
-          src="https://plus.unsplash.com/premium_photo-1661882403999-46081e67c401?q=80&w=1729&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          :src="quiz.img"
           alt="img"
         />
         <div class="card-body">
-          <h2>Programming</h2>
-          <p>2 question</p>
+          <h2>{{ quiz.title }}</h2>
+          <p>{{ quiz.questions.length }} Questions</p>
         </div>
       </div>
     </section>
